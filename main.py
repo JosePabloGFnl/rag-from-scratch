ab5937f95818e2e3ab968ee1a7f143542b5d0260
JosePabloGFnl <jp_glzf@hotmail.com>
2026-09-09 15:56:45 -0600

    sandir reading

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 import os
-import faiss
-import numpy as np
-from sentence_transformers import SentenceTransformer
 
 p = r"data"
 
 articles = []
-for e in os.scandir(p):
-    if e.is_file() and e.name.endswith(".txt"):
-        with open(e.path, "r", encoding="utf-8") as f:
-            text = f.read()
+with os.scandir(p) as entries:
+    for e in entries:
+        if e.is_file() and e.name.endswith(".txt"):
+            with open(e.path, "r", encoding="utf-8") as f:
+                text = f.read()
 
             articles.append({
-    "source": e.name,
-    "text": text
-})
+                "source": e.name,
+                "text": text
+            })
