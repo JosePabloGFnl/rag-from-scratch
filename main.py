b55d420330fdd5a1a05e59125beb3db53124afd6
JosePabloGFnl <jp_glzf@hotmail.com>
2026-09-09 15:42:16 -0600

    File loader

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-import sentence-transformers
-import faiss-cpu
-import numpy
+import os
+import faiss
+import numpy as np
+from sentence_transformers import SentenceTransformer
+
+p = r"data"
+
+articles = []
+for e in os.scandir(p):
+    if e.is_file() and e.name.endswith(".txt"):
+        with open(e.path, "r", encoding="utf-8") as f:
+            text = f.read()
+
+            articles.append({
+    "source": e.name,
+    "text": text
+})
