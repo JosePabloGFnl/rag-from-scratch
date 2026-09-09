541de83144ac675bd32a487c25e2ae4e6af21b33
JosePabloGFnl <jp_glzf@hotmail.com>
2026-09-09 15:58:23 -0600

    file_loader

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import os
 
-p = r"data"
+def file_loader():
+    p = r"data"
 
-articles = []
-with os.scandir(p) as entries:
-    for e in entries:
-        if e.is_file() and e.name.endswith(".txt"):
-            with open(e.path, "r", encoding="utf-8") as f:
-                text = f.read()
+    articles = []
+    with os.scandir(p) as entries:
+        for e in entries:
+            if e.is_file() and e.name.endswith(".txt"):
+                with open(e.path, "r", encoding="utf-8") as f:
+                    text = f.read()
 
-            articles.append({
-                "source": e.name,
-                "text": text
-            })
+                articles.append({
+                    "source": e.name,
+                    "text": text
+                })
